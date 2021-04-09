156adc66f48a751d7a2a74a383a74cb2aca877d7
Josh <josh@duke.edu>
2021-04-09 00:55:45 -0400

    reproducibility_demo_slurm.sh: New!

---
+++ content/misc/reproducibility_demo_slurm.sh
@@ -0,0 +1,50 @@
+#!/usr/bin/env bash
+
+if [ $# -ne 1 ]; then
+    printf "When running this script, please supply an output directory as a command line argument\n" >&2 
+    printf "\n\nNOTE: Memory limitation can cause this to fail at the assignTaxonomy step.\n" >&2
+    printf "This can be a problem if there is not enough memory on the host machine,\n" >&2
+    printf "or if Docker is run with a memory cap. Docker Desktop on MacOS and Windows\n" >&2
+    printf "seems to default to a value that is too low\n\n" >&2
+    exit 1
+else
+    printf "Everything was peachy\n" >&2 
+fi
+
+OUT_DIR=${1}
+BASE_DIR="${OUT_DIR}/reproducible_demo_`date +%s`_tmp"
+
+exit 0
+
+WORK_DIR="$BASE_DIR/work"
+DATA_DIR="$BASE_DIR/data"
+DOCKER_IMAGENAME="ibiem/docker_rstudio_ibiem2020"
+SEP_STRING="\n--------------------------------------------------\n"
+
+echo "Outputting to: $BASE_DIR"
+mkdir -p $WORK_DIR $DATA_DIR
+
+srun git clone https://github.com/ibiem-2020/ibiem_2020_material.git  ${WORK_DIR}/demo
+
+
+printf "\n${SEP_STRING} Pulling docker image: $DOCKER_IMAGENAME ${SEP_STRING}"
+# docker pull $DOCKER_IMAGENAME
+
+
+printf "\n${SEP_STRING} STARTING Pipeline in Singularity ${SEP_STRING}"
+srun singularity exec \
+  -v ${WORK_DIR}:/home/guest \
+  -v ${DATA_DIR}:/data \
+  docker://${DOCKER_IMAGENAME} \
+  Rscript -e "rmarkdown::render('/home/guest/demo/content/lessons/run_everything.Rmd')"
+
+printf "\n${SEP_STRING} FINISHED Pipeline ${SEP_STRING}"
+printf "\n${SEP_STRING} Results output to ${WORK_DIR}/scratch\n\n"
+exit 0
+
+printf "\n${SEP_STRING} To clean up: \n\n"
+echo "rm -rf $BASE_DIR"
+# echo "docker rm -f \`docker ps -aq\`"
+# echo "docker rmi \`docker images -aq\`"
+printf "${SEP_STRING}\n"
+
